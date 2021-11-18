91d9b2d47eeb229d4443dde96436c1005151ef2e
valvegan <valentinavaccari@hotmail.com>
2021-11-18 21:39:34 +0000

    pokemonList array added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-alert("hello world");
+let pokemonList = [{
+    name: "Bulbasaur", 
+    height: 7, 
+    types:["grass", "poison"]},
+    {name: "Charmander", 
+    height: 6, 
+    types: "fire"},
+    {name: "Charmelon", 
+    height: 11, 
+    types: "fire"},
+    {name: "Charizard", 
+    height: 17, 
+    types:["fire", "flying"]},
+];
 
-let favoriteFood = "banana";
-document.write(favoriteFood);
