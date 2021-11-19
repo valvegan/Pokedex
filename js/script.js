81d5bc28642e38fad7702e98e0f4852c5c6fed14
valvegan <valentinavaccari@hotmail.com>
2021-11-19 23:49:33 +0000

    line breaks

--- js/script.js
+++ js/script.js
@@ -23,9 +23,11 @@ let pokemonList = [
 
 //for loop to show pokemon list names and heights
 for (let i = 0; i < pokemonList.length; i++){
-document.write(pokemonList[i].name + " (height " + pokemonList[i].height + ") ")
+document.write(pokemonList[i].name + " (height " + pokemonList[i].height + ") " + "<br>")
 //adding conditional to check if the pokemon height is above 1.2
 if (pokemonList[i].height > 1.2)
 {
-    document.write("Wow, that's a big pokemon!")
+    
+    document.write("Wow, that's a big pokemon!" + "<br>");
+    
 }}
