1670d60f2fde2e5cc50ef0304a601ed0737fe9a9
valvegan <valentinavaccari@hotmail.com>
2021-11-19 23:02:16 +0000

    indentations, exercise 3
    added a for loop to show the list of pokemons with their heights, and then added a conditional to highlight the taller pokemon

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
-let pokemonList = [{
+let pokemonList = [
+    {
     name: "Bulbasaur", 
-    height: 7, 
-    types:["grass", "poison"]},
-    {name: "Charmander", 
-    height: 6, 
-    types: "fire"},
-    {name: "Charmelon", 
-    height: 11, 
-    types: "fire"},
-    {name: "Charizard", 
-    height: 17, 
-    types:["fire", "flying"]},
+    height: 0.7, 
+    types:["grass", "poison"]
+},
+    {
+    name: "Charmander", 
+    height: 0.6, 
+    types: "fire"
+},
+    {
+    name: "Charmelon", 
+    height: 1.1, 
+    types: "fire"
+},
+    {
+    name: "Charizard", 
+    height: 1.7, 
+    types:["fire", "flying"]
+},
 ];
 
+//for loop to show pokemon list names and heights
+for (let i = 0; i < pokemonList.length; i++){
+document.write(pokemonList[i].name + " (height " + pokemonList[i].height + ") ")
+//adding conditional to check if the pokemon height is above 1.2
+if (pokemonList[i].height > 1.2)
+{
+    document.write("Wow, that's a big pokemon!")
+}}
