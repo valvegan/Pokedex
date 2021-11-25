382bb6147ebd48b2a41cdf6d7a374b0b9c427ca3
valvegan <valentinavaccari@hotmail.com>
2021-11-25 20:38:41 +0000

    IIFE
    -wrapped pokemonList into pokemonRepository to avoid accessing global state
    -getAll and add public functions added
    -adjusted forEach() to access the pokemonList inside the pokemonRepository

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-let pokemonList = [
+let pokemonRepository = (function(){
+
+    let pokemonList = [
     {
     name: "Bulbasaur", 
     height: 0.7, 
@@ -18,16 +20,39 @@ let pokemonList = [
     name: "Charizard", 
     height: 1.7, 
     types:["fire", "flying"]
-},
+}
 ];
 
+return {
+    getAll: function() {
+      return pokemonList;
+    },
+    add: function(item) {
+      pokemonList.push(item);
+    },
+    
+  };
+
+})();
+
+pokemonRepository.add({ 
+
+    name: 'Pikachu', 
+    height: 0.4,
+    types: "electric"
+
+});
+//returning the array into the console
+console.log(pokemonRepository.getAll()); 
+
 //forEach() loop insteaf of the for loop
-pokemonList.forEach(function(pokemon){
-    document.write(pokemon.name + " (height " + pokemon.height + ") " + "<br>")
+
+pokemonRepository.getAll().forEach(function(item){
+    document.write(item.name + " (height " + item.height + ") " + "<br>")
 
 //adding conditional to check if the pokemon height is above 1.2
 
-if (pokemon.height > 1.2)
+if (item.height > 1.2)
 {
     
     document.write("Wow, that's a big pokemon!" + "<br>");
